8722ac7112c5ef30f86d9e759f8639b48ce760d3
kr-sharma28 <krishnapurohit417400@gmail.com>
2025-01-24 01:07:49 +0530

    palindrome

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 console.log("My first repo");
 console.log("My second repo");
 console.log("My version 1.1.1.1.0")
+
+let str = "rececar"
+function checkpalindrome(str) {
+    let bag = "";
+    for (let i=str.length-1;i>=0;i--){
+bag = bag + str[i];
+    }
+    if(bag==str){
+        return true;
+    }else{
+        return false;
+    }
+}
